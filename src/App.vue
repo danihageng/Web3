fa6d8b9ca8ad7f7294eb0899e65c93b67ad70c30
Daniel Hagen <danihagen14@gmail.com>
2022-11-29 12:13:36 +0100

    update code first commit web3ViteVue

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { useCryptoStore } from '~/stores/crypto'
+
+const props = useCryptoStore()
+
+// https://github.com/vueuse/head
+// you can use this to manipulate the document head in any components,
+// they will be rendered correctly in the html results with vite-ssg
+useHead({
+  title: 'Daniel Crypto Project',
+  meta: [
+    { name: 'description', content: 'Daniel Crypto Project' },
+  ],
+})
+
+</script>
+
+<template>
+  <!-- {{ props.loading }} -->
+  <Loader v-if="props.loading" />
+  <router-view />
+</template>
